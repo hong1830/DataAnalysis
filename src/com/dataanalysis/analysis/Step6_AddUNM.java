288eac1d1b2223b61435a52c809d826ad8e1ca8d
= <=>
2014-03-26 15:02:54 +0800

    modify error

--- src/com/dataanalysis/analysis/Step6_AddUNM.java
+++ src/com/dataanalysis/analysis/Step6_AddUNM.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
